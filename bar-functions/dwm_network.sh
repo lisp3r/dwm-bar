beacd553bb6eb9038c96e633dae886fb136f3ff9
nowaday <nowaday@github.com>
2020-11-18 14:47:00 +0300

    Update dwm_network.sh

--- bar-functions/dwm_network.sh
+++ bar-functions/dwm_network.sh
@@ -2,26 +2,20 @@
 
 # A dwm_bar function to show the current network connection/SSID, private IP, and public IP
 # Joe Standring <git@joestandring.com>
+# lisp3r <git@lisp3r.com>
 # GNU GPLv3
 
-# Dependencies: NetworkManager, curl
+# Dependencies: nmcli
+ wifi_icon=""
 
-dwm_network () {
-    CONNAME=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}')
-    if [ "$CONNAME" = "" ]; then
-        CONNAME=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)
+dwm_network() {
+    wifi=$(nmcli connection show | grep -v -e "--" | grep -v NAME | awk '{ if ($3 == "wifi") {print $1} }')
+
+    if [ ! -z "$wifi" ]; then
+        printf "%s" "$SEP1"
+        printf "%s %s" $wifi_icon "$wifi"
+        printf "%s" "$SEP2"
     fi
-
-    PRIVATE=$(nmcli -a | grep 'inet4 192' | awk '{print $2}')
-    PUBLIC=$(curl -s https://ipinfo.io/ip)
-
-    printf "%s" "$SEP1"
-    if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "🌐 %s %s | %s" "$CONNAME" "$PRIVATE" "$PUBLIC"
-    else
-        printf "NET %s %s | %s" "$CONNAME" "$PRIVATE" "$PUBLIC"
-    fi
-    printf "%s\n" "$SEP2"
 }
 
 dwm_network
