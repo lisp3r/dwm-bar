1745b8c369291b96a08756e5c65f215a60a193e6
nowaday <nowaday@github.com>
2020-11-18 17:22:57 +0300

    Update battery

--- bar-functions/dwm_battery.sh
+++ bar-functions/dwm_battery.sh
@@ -11,15 +11,33 @@ declare -A batt_states=( ["Unknown"]="unkn"
                          ["Not charging"]="nchrg"
                          ["Full"]="full")
 
-bat_icon=""
+icon_full=""
+icon_three_quarters=""
+icon_half=""
+icon_quarter=""
+icon_empty=""
 
 dwm_battery () {
     bat_st="$(cat /sys/class/power_supply/BAT0/status)"
     bat_cap="$(cat /sys/class/power_supply/BAT0/capacity)"
 
+    if [[ bat_cap -le 10 ]]; then
+        icon=$icon_empty
+    elif [[ bat_cap -gt 10 && bat_cap -le 25 ]]; then
+        icon=$icon_quarter
+    elif [[ bat_cap -gt 25 && bat_cap -le 50 ]]; then
+        icon=$icon_half
+    elif [[ bat_cap -gt 50 && bat_cap -le 90 ]]; then
+        icon=$icon_three_quarters
+    elif [[ bat_cap -gt 90 && bat_cap -le 100 ]]; then
+        icon=$icon_full
+    fi
+
     printf "%s" "$SEP1"
-    printf "%s %s%% %s" $bat_icon "${bat_cap}" "${batt_states[$bat_st]}"
+    printf "%s %s%%" $icon "${bat_cap}"
+    # printf "%s %s%% %s" $icon "${bat_cap}" "${batt_states[$bat_st]}"
     printf "%s" "$SEP2"
+
 }
 
 dwm_battery
