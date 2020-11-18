137c91e5de8510981f9b6b4817e3cdf1c44cc0c9
nowaday <nowaday@github.com>
2020-11-18 14:46:10 +0300

    Update dwm_keyboard.sh

--- bar-functions/dwm_keyboard.sh
+++ bar-functions/dwm_keyboard.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 
 # A dwm_bar function that displays the current keyboard layout
-# lisp3r
+# Joe Standring <git@joestandring.com>
+# lisp3r <git@lisp3r.com>
 # GNU GPLv3
-
 
 EN_MASK="00000000"
 RU_MASK="00001000"
 
+declare -A keyboard_states=( ["00000000"]="en"
+                         ["00001000"]="ru")
+
+icon=""
+
 dwm_keyboard () {
-    KEY_MASK=$(xset -q|grep LED| awk '{ print $10 }')
+    key_mask=$(xset -q|grep LED| awk '{ print $10 }')
 
     printf "%s" "$SEP1"
-    if [ "$KEY_MASK" = "$EN_MASK" ]; then
-        printf "%s" "en"
-    elif [ "$KEY_MASK" = "$RU_MASK" ]; then
-        printf "%s" "ru"
-    fi
-    printf "%s\n" "$SEP2"
+    printf "%s %s" $icon "${keyboard_states[$key_mask]}"
+    printf "%s" "$SEP2"
 }
 
 dwm_keyboard
