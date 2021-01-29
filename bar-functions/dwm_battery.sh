23af7f37a988ab87f149bb9f66c492f32767749f
nowaday <nowaday@github.com>
2021-01-29 10:30:35 +0300

    enhancement batt

--- bar-functions/dwm_battery.sh
+++ bar-functions/dwm_battery.sh
@@ -6,11 +6,11 @@
 # GNU GPLv3
 
 dwm_battery () {
-    declare -A batt_states=( ["Unknown"]="unkn"
-                             ["Charging"]="chrg"
-                             ["Discharging"]="dischr"
-                             ["Not charging"]="nchrg"
-                             ["Full"]="full")
+    # declare -A batt_states=( ["Unknown"]="unkn"
+    #                          ["Charging"]="chrg"
+    #                          ["Discharging"]="dischr"
+    #                          ["Not charging"]="nchrg"
+    #                          ["Full"]="full")
 
     icon_full=
     icon_three_quarters=
@@ -23,18 +23,18 @@ dwm_battery () {
     bat_st=$(cat $BAT_STAT/status)
     bat_cap=$(cat $BAT_STAT/capacity)
 
-    if [[ bat_cap -le 10 ]]; then
+    if [ "$bat_cap" -le 10 ]; then
         icon=$icon_empty
-        if [[ $bat_st != 'Charging' ]]; then
+        if [ "$bat_st" != 'Charging' ]; then
             notify-send "               You are about to loosing your job! Plug in a power cabel!!"
         fi
-    elif [[ bat_cap -gt 10 && bat_cap -le 25 ]]; then
+    elif [ "$bat_cap" -gt 10 ] && [ "$bat_cap" -le 25 ]; then
         icon=$icon_quarter
-    elif [[ bat_cap -gt 25 && bat_cap -le 50 ]]; then
+    elif [ "$bat_cap" -gt 25 ] && [ "$bat_cap" -le 50 ]; then
         icon=$icon_half
-    elif [[ bat_cap -gt 50 && bat_cap -le 90 ]]; then
+    elif [ "$bat_cap" -gt 50 ] && [ "$bat_cap" -le 90 ]; then
         icon=$icon_three_quarters
-    elif [[ bat_cap -gt 90 && bat_cap -le 100 ]]; then
+    elif [ "$bat_cap" -gt 90 ] && [ "$bat_cap" -le 100 ]; then
         icon=$icon_full
     fi
 
